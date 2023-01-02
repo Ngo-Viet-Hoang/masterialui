29fefd40f339ad3e7e510085636403cb28dc28d0
ADMIN <ADMIN@DESK^Cgit config --global user.email ADMIN@DESK^C>
2023-01-02 17:14:14 +0700

    Update lại code CommonButton, Thêm Form Add User và tìm kiếm theo dữ liệu đã có

--- src/pages/Authentication/Authentication.js
+++ src/pages/Authentication/Authentication.js
@@ -1,67 +1,102 @@
-import React from "react";
-import Grid from "@mui/material/Grid";
+import React, { useState } from "react";
 import BasicCard from "../../components/common/BasicCard/BasicCard";
 import SearchBar from "../../components/common/SearchBar/SearchBar";
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import IconButton from '@mui/material/IconButton';
-import { cardHeaderStyles } from './styles';
-import CommonButton from '../../components/common/CommonButton/CommonButton';
-import RefreshIcon from '@mui/icons-material/Refresh';
-import GridWrapper from '../../components/common/GridWrapper/GridWrapper';
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import { cardHeaderStyles } from "./styles";
+import CommonButton from "../../components/common/CommonButton/CommonButton";
+import RefreshIcon from "@mui/icons-material/Refresh";
+import GridWrapper from "../../components/common/GridWrapper/GridWrapper";
+import NewUserModal from "../../components/common/Modal/NewUserModal";
 
 const Authentication = () => {
+    const [open, setOpen] = useState(false);
+    const [users, setUsers] = useState([]);
+    const [searchResults, setSearchResults] = useState(users);
 
-  const getHeader = () => {
-      const handleChange = (value) => {
-          console.log(value);
-      };
+    const getHeader = () => {
+        const handleSearch = (value) => {
+            filterData(value);
+        };
 
-      const addUser = () => {
-          console.log('click')
-      };
+        const filterData = (value) => {
+            const lowercasedValue = value.toLowerCase().trim();
+            if (lowercasedValue === '') setUsers(searchResults);
+            else {
+                const filteredData = searchResults.filter((item) => {
+                    return Object.keys(item).some((key) => 
+                    item[key].toString().toLowerCase().includes(lowercasedValue)
+                    );
+                });
+                setUsers(filteredData)
+            };
+        };
 
-      return (
-          <Box sx={cardHeaderStyles.wrapper}>
-              <SearchBar 
-                  placeholder="Search by email address, phone number, or user UID"
-                  onChange={(event) => handleChange(event.target.value)}
-                  searchBarWidth='720px'
-              />
-              <Box>
-                  <CommonButton 
-                      variant="contained"
-                      onClick={addUser}
-                      size="large"
-                      sx={cardHeaderStyles.addUserButton}
-                  >
-                      Add user
-                  </CommonButton>
-                  <IconButton>
-                      <RefreshIcon />
-                  </IconButton>
-              </Box>
-          </Box>
-      )
-  };
+        const addUser = () => {
+            setOpen(true);
+        };
 
-  const getContent = () => (
-      <Typography 
-          align="center"
-          sx={{ margin: '40px 16px', color: 'rgba(0, 0, 0, 0.6)', fontSize: '1.3rem'}}
-      >
-          No users for this project yet
-      </Typography>
-  );
+        return (
+            <Box sx={cardHeaderStyles.wrapper}>
+                <SearchBar 
+                    placeholder="Search by email address, phone number, or user UID"
+                    onChange={(event) => handleSearch(event.target.value)}
+                    searchBarWidth='720px'
+                />
+                <Box>
+                    <CommonButton 
+                        variant="contained"
+                        onClick={addUser}
+                        size="large"
+                        sx={cardHeaderStyles.addUserButton}
+                    >
+                        Add user
+                    </CommonButton>
+                    <IconButton>
+                        <RefreshIcon />
+                    </IconButton>
+                </Box>
+            </Box>
+        )
+    };
 
-  return (
-      <GridWrapper>
-          <BasicCard
-              header={getHeader()}
-              content={getContent()}
-          />
-      </GridWrapper>
-  )
+    const addNewUser = (data) => {
+        users.push({ ...data });
+        setOpen(false);
+    };
+
+    const getContent = () => (
+        <>
+            {
+                users.length ? 
+                    users.map((user) => (
+                        <Box sx={{ marginBottom: '20px' }}>
+                            <Typography>User ID: {user.userId}</Typography>
+                            <Typography>Email: {user.email}</Typography>
+                            <Typography>Phone Number: {user.phoneNumber}</Typography>
+                        </Box>
+                    )) :
+                    <Typography 
+                        align="center"
+                        sx={{ margin: '40px 16px', color: 'rgba(0, 0, 0, 0.6)', fontSize: '1.3rem'}}
+                    >
+                        No users for this project yet
+                    </Typography>
+            }
+        </>
+    );
+
+    return (
+        <GridWrapper>
+            <BasicCard
+                header={getHeader()}
+                content={getContent()}
+            />
+            <NewUserModal open={open} onClose={() => setOpen(false)} addNewUser={addNewUser}/>
+        </GridWrapper>
+    )
 }
+
 
 export default Authentication;
