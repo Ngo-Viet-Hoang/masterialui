7b4e8c0ef25bbb222ef032c1f0da9be55bc45fe0
ADMIN <ADMIN@DESK^Cgit config --global user.email ADMIN@DESK^C>
2023-01-03 16:22:56 +0700

    Update Snackbar and DataGrid

--- src/pages/MachineLearning.js
+++ src/pages/MachineLearning.js
@@ -1,10 +1,38 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
+import GridWapper from '../components/common/GridWrapper/GridWrapper';
+import BasicSnackbar from "../components/common/BasicSnackbar/BasicSnackbar";
+import CommonButton from "../components/common/CommonButton/CommonButton";
 
 const MachineLearning = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
   return (
-    <div style={{ marginLeft: "500px" }}>This is MachineLearning page.</div>
-  );
-};
+    <GridWapper>
+      This is MachineLearning page
+      <CommonButton variant="contained" onClick={handleClick}>
+        Open success snackbar
+      </CommonButton>
+      <BasicSnackbar
+      open = {open}
+      onClose = {handleClose}
+      severity = "error"
+      message= "Error Msg"
+      />
+    </GridWapper>
+  )
+}
 
 export default MachineLearning;
--- src/pages/Storage.js
+++ src/pages/Storage.js
@@ -1,8 +1,36 @@
 import React from "react";
-import Grid from "@mui/material/Grid";
-
+import GridWapper from '../components/common/GridWrapper/GridWrapper';
+import BasicSnackbar from "../components/common/BasicSnackbar/BasicSnackbar";
+import BasicCard from "../components/common/BasicCard/BasicCard";
+import UserTable from "../components/UserTable/UserTable";
 const Storage = () => {
-  return <div style={{ marginLeft: "500px" }}>This is storage page.</div>;
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  return (
+    <GridWapper>
+     <BasicCard 
+          content={<UserTable onError={() => setOpen(true)} />}
+        />
+        <BasicSnackbar
+          open={open}
+          severity="error"
+          message="Data couldn't be fetched"
+          onClose={handleClose}
+        />
+    </GridWapper>
+  )
 };
 
 export default Storage;
